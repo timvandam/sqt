eb1d6729b92861a2e1b5942e4de76154f8cb6716
Tim van Dam <timvandamcs@gmail.com>
2020-04-19 19:43:52 +0200

    initialized with undefined

--- src/codingbat/blackjack/blackjack.ts
+++ src/codingbat/blackjack/blackjack.ts
@@ -1,10 +1,9 @@
 /**
  * Computes the number closest to, but not over 21
  */
-export function blackjack (a: number, b: number): number {
-  const nums = [a, b]
-  let closest
-  let current
+export function blackjack (...nums: number[]): number {
+  let closest = undefined
+  let current = undefined
   while (nums.length && (current = nums.shift())) {
     if (current > 21) continue
     if (closest === undefined || (21 - current) < (21 - closest)) closest = current

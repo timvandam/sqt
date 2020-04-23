5f137da13ca7db66a6f2cd0c707cefd53287b1aa
Tim van Dam <timvandamcs@gmail.com>
2020-04-23 21:27:59 +0200

    finished the first part of lecture 2 (specificication-based testing)

--- src/roman/romanNumerals.ts
+++ src/roman/romanNumerals.ts
@@ -12,7 +12,7 @@ const alphabet = new Map<string, number>([
  * Converts a roman numeral to a base 10 integer
  * @param {string} roman - the roman numeral to convert to a base 10 integer
  */
-export function romanToInteger (roman: string): number {
+export function romanToInteger (roman: string): number4 {
   roman = roman.toUpperCase()
   let total = 0
   for (let i = 0; i < roman.length; i++) {
